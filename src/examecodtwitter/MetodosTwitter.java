2cc9a0a2b478bf58fcfd0ffb168947fd66781ec0
gpineiroblanco <gpineiroblanco@eniac16.danielcastelao.org>
2015-03-16 09:33:39 +0100

    Constructor, da acceso a nuestra cuenta en twitter.

---
+++ src/examecodtwitter/MetodosTwitter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examecodtwitter;
+
+import twitter4j.Twitter;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ *
+ * @author gpineiroblanco
+ */
+public class MetodosTwitter {
+    
+    ConfigurationBuilder cb = new ConfigurationBuilder();
+    Twitter twitter;
+    
+     /**
+     * Constructor, da acceso a nuestra cuenta en twitter.
+     */
+}
