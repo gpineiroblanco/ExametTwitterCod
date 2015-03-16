acc17e42a8fe742729b52cb9719a89a6390ffe8b
gpineiroblanco <gpineiroblanco@eniac16.danielcastelao.org>
2015-03-16 09:34:12 +0100

    Hacemos el timeLine

--- src/examecodtwitter/MetodosTwitter.java
+++ src/examecodtwitter/MetodosTwitter.java
@@ -6,6 +6,7 @@
 package examecodtwitter;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
@@ -19,5 +20,18 @@ public class MetodosTwitter {
     
      /**
      * Constructor, da acceso a nuestra cuenta en twitter.
+     */
+    public MetodosTwitter() {
+
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("kVeUkuBbS7E4m2BzzqKTCa00t")
+                .setOAuthConsumerSecret("znSSjhVZhkjBBsNy9C7YSgclTO14eFFz0FXYBlCGxb0bKn3hXC")
+                .setOAuthAccessToken("180784730-3sKIMf2JHiL0DnzDoGQ0Lzn2ES4xFo5bTOTIrVtv")
+                .setOAuthAccessTokenSecret("kTXQAcbRbzBPi6pjttzgxEU9AhH6USs5uaX8qm1JxuD6S");
+        twitter = new TwitterFactory(cb.build()).getInstance();
+    }
+
+    /**
+     * Nos da nuestro timeLine
      */
 }
